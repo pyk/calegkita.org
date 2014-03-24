7bb6bdf0a94ee289663656407fb60c3fca1ba1d4
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2014-03-24 19:39:33 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,20 +26,64 @@ app.config(["$routeProvider",
         redirectTo: "/"
       });
 }]);
-app.controller("DapilCtrl", ["$scope", "$http", function($s,$h) {
+app.controller("DaftarCalegCtrl",
+  ["$scope", "$routeParams", "$http", "cfpLoadingBar", "$timeout",
+  function ($s,$rp,$h, cfpLoadingBar, $timeout) {
+    $s.daftarCaleg = [];
+    $s.dapil = {};
+    $s.provinsi = {};
+    $s.statusContent = true;
+
+    var url = "http://api.pemiluapi.org/candidate/api/caleg";
+    $h.get(url, {params: {
+      apiKey: "40381f1d8123102fd74d85aef44d70d5",
+      dapil: $rp.id
+        }})
+      .success(function (data) {
+        $s.daftarCaleg = data.data.results.caleg;
+        $s.dapil = $s.daftarCaleg[0].dapil
+        $s.provinsi = $s.daftarCaleg[0].provinsi
+        if ($s.dapil) {
+          $s.statusContent = false;
+        }
+      });
+    
+    // $s.start = function() {
+    //   cfpLoadingBar.start();
+    // };
+
+    // $s.complete = function () {
+    //   cfpLoadingBar.complete();
+    // };
+
+    
+    // $s.start();
+    // $timeout(function() {
+    //   $s.complete();
+    //   $s.statusContent = false;
+    // }, 1250);
+}]);
+app.controller("DapilCtrl", 
+  ["$scope", "$http", "cfpLoadingBar", "$timeout",
+  function ($s,$h, cfpLoadingBar, $timeout) {
+  $s.statusContent = true;
+  $s.semuaDapil = [];
   $s.getDapil = function () {
     var key = "40381f1d8123102fd74d85aef44d70d5";
     var url = "http://api.pemiluapi.org/candidate/api/dapil";
     $h.get(url, {params: {apiKey: key, callback: "JSON_CALLBACK"}})
       .success(function(data,status, header, config){
         $s.semuaDapil = data.data.results.dapil;
+        if ($s.semuaDapil) {
+          $s.statusContent = false;
+        }
       })
       .error(function(data,status,header,config) {
         
       });
   };
-
   $s.getDapil();
+  
 }]);
 
 var cuk = "as";
