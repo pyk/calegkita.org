44c70abfaa5548d33babfb356d5be8e69d6ce1b7
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2014-03-28 21:53:15 +0700

    dapil: add loading status and error message

--- src/app/controller/dapil.controller.js
+++ src/app/controller/dapil.controller.js
@@ -1,15 +1,23 @@
-app.controller("DapilCtrl", ["$scope", "$http", function($s,$h) {
+app.controller("DapilCtrl", 
+  ["$scope", "$http",
+  function ($s,$h) {
+  $s.statusContent = true;
+  $s.semuaDapil = [];
   $s.getDapil = function () {
     var key = "40381f1d8123102fd74d85aef44d70d5";
     var url = "http://api.pemiluapi.org/candidate/api/dapil";
     $h.get(url, {params: {apiKey: key, callback: "JSON_CALLBACK"}})
-      .success(function(data,status, header, config){
+      .success(function (data,status, header, config) {
         $s.semuaDapil = data.data.results.dapil;
+        if ($s.semuaDapil) {
+          $s.statusContent = false;
+        }
       })
-      .error(function(data,status,header,config) {
-        
+      .error(function (data,status,header,config) {
+        var loadingStatus = document.getElementsByClassName("loading-status")[0];
+        loadingStatus.textContent = "Tidak bisa mengambil data dari server, periksa koneksi internet anda.";
       });
   };
-
   $s.getDapil();
+  
 }]);
